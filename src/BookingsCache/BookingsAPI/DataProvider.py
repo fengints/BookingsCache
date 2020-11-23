8cf63216f8af9a28ca2f8a35a801c912d16661f0
Mukhammad <Fengints@yandex.ru>
2020-11-23 22:42:28 +0600

    refactoring dataprovider

--- src/BookingsCache/BookingsAPI/DataProvider.py
+++ src/BookingsCache/BookingsAPI/DataProvider.py
@@ -7,30 +7,31 @@ from datetime import timedelta
 class DataProvider:
     def __init__(self, max_workers = 30):
         self.session = FuturesSession(max_workers)
+        self.url = 'https://api.skypicker.com/flights'
+
     def GetDataForNextMonth(self):
         pass
     def GetDataBy(self, fly_from, fly_to, date_from, date_to, response_handler = None):
-        url = 'https://api.skypicker.com/flights'
         response_list = []
 
         PARAMS = {'fly_from':fly_from, 'fly_to': fly_to, "partner":"picky"}
 
-        '''Move to utils'''
-        def isDateEqual(date1, date2):
-            if (date1.day != date2.day):
-                return False
-            if (date1.month != date2.month):
-                return False
-            if (date1.year != date2.year):
-                return False
-            return True
+        handler = lambda curday:(
+            self.on_loop_operation(
+                self.url, PARAMS,
+                curday, response_list))
 
-        def Loop_over_dates(func, date_start, date_end):
+        self.loop_over_dates(handler,
+                                date_from,
+                                date_to)
+        return response_list
+        
+    def loop_over_dates(self, on_loop: function, date_start, date_end):
             curday = date_start
 
             while (True):
                 #perform operations
-                func(curday)
+                on_loop(curday)
 
                 #add next day
                 curday += timedelta(days=1)
@@ -38,15 +39,8 @@ class DataProvider:
                 #exit if day is the same
                 if (isDateEqual(curday, date_end)):
                     break
-        
-        Loop_over_dates(lambda curday:self.operation(url, PARAMS, curday, response_list = response_list),
-                                date_from,
-                                date_to)
-        return response_list
-        
-
     
-    def operation(self, url, params, desired_date: datetime, response_list):
+    def on_loop_operation(self, url, params, desired_date: datetime, response_list):
             parameters = params.copy()
             
             parameters.update(
@@ -56,3 +50,13 @@ class DataProvider:
             action_item = self.session.get(url, params = parameters,)# hooks = {'response' : response_handler})
 
             response_list.append(action_item)
+
+    '''Move to utils'''
+    def isDateEqual(date1, date2):
+            if (date1.day != date2.day):
+                return False
+            if (date1.month != date2.month):
+                return False
+            if (date1.year != date2.year):
+                return False
+            return True   
--- src/BookingsCache/BookingsCache/__init__.py
+++
@@ -1,4 +0,0 @@
-#celery scheduler
-from .celery import app as celery_app
-
-__all__ = ('celery_app',)
